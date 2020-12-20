86bb553e389f2a1359788ef700f6a09e7c691a5f
tapixhx <tapish818@gmail.com>
2020-12-20 12:16:34 +0530

    Send message and Leave room

--- server/app.js
+++ server/app.js
@@ -19,20 +19,26 @@ io.on('connection', (socket) => {
       socket.broadcast.to(data.room).emit('new user joined', {user:data.user, message:'has joined this room'});
   });
 
-  socket.on('message', (msg) => {
-    console.log('message: ' + msg);
+  socket.on('leave', function(data) {
+    console.log(data.user + ' left the room '+data.room);
+    socket.broadcast.to(data.room).emit('left room', {user:data.user, message:'has left this room'});
+    socket.leave(data.room);
+  });
+
+  socket.on('message', function(data) {
+    io.in(data.room).emit('new message', {user:data.user, message:data.message});
   });
 
 });
 
-io.on('connection', (socket) => {
+// io.on('connection', (socket) => {
 
-    socket.on('my message', (msg) => {
-        console.log(msg);
-      io.emit('my broadcast', `server: ${msg}`);
-    });
+//     socket.on('my message', (msg) => {
+//         console.log(msg);
+//       io.emit('my broadcast', `server: ${msg}`);
+//     });
 
-});
+// });
 
 http.listen(3000, () => {
   console.log('listening on :3000');
